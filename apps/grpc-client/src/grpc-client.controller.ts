ea47cf900386813a74f8e52efb6e367706ce1f86
zengjc <2356924146@qq.com>
2023-12-10 23:05:34 +0800

    feat: 添加类型

--- apps/grpc-client/src/grpc-client.controller.ts
+++ apps/grpc-client/src/grpc-client.controller.ts
@@ -2,32 +2,30 @@ import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { GrpcClientService } from './grpc-client.service';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Metadata } from '@grpc/grpc-js';
-// import * as grpc from '@grpc/grpc-js';
-// import * as protoLoader from '@grpc/proto-loader';
-// import { join } from 'path';
-// import HeroesService from './proto/hero.proto';
-// console.log(HeroesService);
 
-// 加载.proto文件
-// const packageDefinition = protoLoader.loadSync(
-//   join(__dirname, '/proto/hero.proto'),
-// );
-// const grpcObject = grpc.loadPackageDefinition(packageDefinition);
-// const { HeroesService } = grpcObject.hero;
-// console.log('grpcObject', grpcObject);
+interface HeroById {
+  id: number;
+}
+interface Hero {
+  id: number;
+  name: string;
+}
+interface HeroesService {
+  findOne: (heroById: HeroById, metadata: Metadata) => Hero;
+}
 
 @Controller()
 export class GrpcClientController implements OnModuleInit {
-  private heroesService;
+  private heroesService: HeroesService;
   constructor(
     private readonly grpcClientService: GrpcClientService,
     @Inject('HERO_SERVICE') private client: ClientGrpc,
   ) {}
   onModuleInit() {
-    this.heroesService = this.client.getService('HeroesService');
+    this.heroesService = this.client.getService<HeroesService>('HeroesService');
   }
   @Get()
-  getHero(): string {
+  getHero(): Hero {
     // 第二个参数可以传递元数据
     const metadata = new Metadata();
     metadata.add('Set-Cookie', 'yummy_cookie=choco');
